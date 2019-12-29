ac25790c9e9e5da4a0b293a78d8952750f7b04ac
Dong Soo Michael Seo <ds2242@cornell.edu>
2019-12-29 22:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,18 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (whisper(string) === string){
+<<<<<<< HEAD
     return "I can\'t hear you!"
   }else if(shout(string) === string){
     return "YES INDEED!"
   }else if(string ==="I love you, Grandma."){
     return "I love you, too."
   }  
+=======
+    return "I can\'t not here you!"
+  }else if(shout(string) === string){
+    return "YES INDEED!"
+  }else if(if string =)  
+>>>>>>> a1ebb3919568a93e1393406dab5245a00e43b818
 }
 
